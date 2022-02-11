2deef16f5498f1837a49d670cab8ef016935e86e
HR (HONEY) <HR (HONEY)@DESKTOP-CLONB7B>
2022-02-11 22:10:35 +0530

    update

--- java learning/src/simpleprogramm/newjava.java
+++ java learning/src/simpleprogramm/newjava.java
@@ -3,7 +3,8 @@ package simpleprogramm;
 public class newjava {
 
 	public static void main(String[] args) {
-		System.out.println("sitaram");
+		System.out.println("jay sitaram");
 	}
 
 }
+//  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
