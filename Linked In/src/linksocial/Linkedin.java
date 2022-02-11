c5c8bf1a50a3ca92a35c28a74bb3520452408211
HR (HONEY) <HR (HONEY)@DESKTOP-CLONB7B>
2022-02-11 22:25:10 +0530

    Linked In

---
+++ Linked In/src/linksocial/Linkedin.java
@@ -0,0 +1,20 @@
+package linksocial;
+
+public class Linkedin {
+
+	
+	 public void Name(String s1) {
+		 System.out.println("Your Name Is" + s1);
+	 }
+	 
+	 public void Email (String email) {
+		 System.out.println("Your Email Is" + email);
+	 }
+	 
+	 public void Number(long number) {
+		 System.out.println("Your Number Is" + number);
+	 }
+
+}
+
+
---
+++ Linked In/src/linksocial/Linkedin2.java
@@ -0,0 +1,23 @@
+package linksocial;
+
+public class Linkedin2 extends Linkedin {
+
+	public static void main(String[] args) {
+		
+		Linkedin2 Linkedin2 = new Linkedin2 ();
+		System.out.println("LINKED IN WEBSITE");
+		Linkedin2.Name(" Himanshu Puriohit");
+		Linkedin2.Email("purooihit@gmail.com");
+		Linkedin2.Number(  +966090746);
+		Linkedin2.address("Mavli");
+		
+	}
+
+	private void address(String address) {
+		System.out.println("Your Address Is " +address);
+		
+	}
+	
+	
+
+}
