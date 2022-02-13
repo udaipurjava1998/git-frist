cbb07925c284b531ddf7fd581af3c02b6d4fc920
HR (HONEY) <HR (HONEY)@DESKTOP-CLONB7B>
2022-02-13 22:39:24 +0530

    class

---
+++ variable/src/variable/abcDemo.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class abcDemo {
+
+	static int i = 10;
+	int j = 5;
+
+	public static void main(String[] args) {
+
+		int k = 2;
+		System.out.println(i);
+
+		abcDemo abcDemo = new abcDemo();
+		System.out.println(abcDemo.j);
+
+		System.out.println(k);
+	}
+
+}
